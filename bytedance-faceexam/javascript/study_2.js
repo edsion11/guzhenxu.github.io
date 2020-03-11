ff1be69eac8ba58549c547cb81629031b08f7042
edsion11 <1447424545@qq.com>
2020-03-11 11:12:04 +0800

    2

--- bytedance-faceexam/javascript/study_2.js
+++ bytedance-faceexam/javascript/study_2.js
@@ -28,4 +28,21 @@ function list(){
 var li = new list;
 li.data[0]=1;
 li.data[1]=2
-console.log(li.data)
+//console.log(li.data)
+function ishuiwen(num){
+    if(num<0) return false
+    return num.toString() == num.toString().split("").reverse().join(""); 
+}
+//console.log(ishuiwen(-12321))
+function fanzhuan(num){
+    if(num>=0){
+        return num.toString().split("").reverse().join("")
+    }
+    else{
+        var s = num.toString().substr(1,num.toString().length)
+        //console.log(s)
+        return "-"+ s.toString().split("").reverse().join("")
+    }
+    
+}
+console.log(fanzhuan(-12223))
