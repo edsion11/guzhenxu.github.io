4caf93da759c00fbdbee02ea4c9fff39f83a3cf2
edsion11 <1447424545@qq.com>
2020-05-06 11:42:10 +0800

    Promise完结

--- 日常学习更/javascript/异步javascript/promise.js
+++ 日常学习更/javascript/异步javascript/promise.js
@@ -1,15 +1,30 @@
-const p = new Promise((resolve, reject) => {
-  console.log`创建好了promise对象`
-  /*setTimeout(() => {
-    console.log(new Date())
-    resolve(new Date())
-  }, 1000)*/
-  resolve(new Date().getHours())
+new Promise((resolve, reject) => {
+  //这里是同步执行
+  console.log`先指定回调函数，在改变状态`
+  setTimeout(() => {
+    resolve(1) //再改变pedding状态，同时改变数据
+  }, 1000)
+}).then((value) => {
+  //先指定回调函数
+  console.log('value1---->' + value)
 })
-p.then(console.log)
-console.log`走你`
+//先改变状态，再指定函数
+//第一种，去掉异步的定时器
+new Promise((resolve, reject) => {
+  console.log`//先改变状态，再指定函数`
+  resolve(2)
+}).then((value) => {
+  console.log('value2--->' + value)
+})
+//第二种都设置定时器
+const p3 = new Promise((resolve, reject) => {
+  console.log`//先改变状态，再指定函数`
+  setTimeout(() => {
+    resolve(3)
+  }, 1000)
+})
 setTimeout(() => {
-  //setTimeout函数本身就是一个异步执行函数
-  console.log('settimeout')
-}, 2000)
-console.log('我是setTimeout后面的log()')
+  p3.then((value) => {
+    console.log('value3--->' + value)
+  })
+}, 1200)
--- 日常学习更/javascript/异步javascript/手写Promise.js
+++ 日常学习更/javascript/异步javascript/手写Promise.js
@@ -22,6 +22,7 @@
         })
       }
     }
+
     function reject(reason) {
       if (that.status !== 'pending') {
         return
@@ -44,15 +45,143 @@
       reject(error)
     }
   }
+
   myPromise.prototype.then = function (onResolved, onRejected) {
+    //绑定this
     const that = this
-    that.callbacks.push({ onResolved, onRejected })
+    //value传递
+    onResolved =
+      typeof onResolved === 'function' ? onResolved : (value) => value
+
+    //指定默认的失败的回调，实现异常穿透
+    onRejected =
+      typeof onRejected === 'function'
+        ? onRejected
+        : (reason) => {
+            throw reason //不能return,要传递失败的状态
+          }
+
+    //that.callbacks.push({ onResolved, onRejected })
+    return new myPromise((resolve, reject) => {
+      function handle(callback) {
+        try {
+          const result = callback(that.data)
+          if (result instanceof Promise) {
+            result.then(
+              (value) => resolve(value),
+              (reason) => reject(reason)
+            )
+          } else {
+            resolve(result)
+          }
+        } catch (error) {
+          reject(error)
+        }
+      }
+      if (that.status === 'pending') {
+        that.callbacks.push({
+          onResolved(value) {
+            handle(onResolved)
+          },
+          onRejected(reason) {
+            handle(onRejected)
+          },
+        })
+      } else if (that.status === 'fullFilled') {
+        setTimeout(() => {
+          handle(onResolved)
+        })
+      } else {
+        setTimeout(() => {
+          handle(onRejected)
+        })
+      }
+    })
   }
-  myPromise.prototype.catch = function (onRejected) {}
-  myPromise.resolve = function (value) {}
-  myPromise.reject = function (reason) {}
-  myPromise.all = function (arr) {}
-  myPromise.race = function () {}
+
+  myPromise.prototype.catch = function (onRejected) {
+    return this.then(undefined, onRejected)
+  }
+  /* 
+  const p1 = Promise.resolve(2)
+  const p2 = Promise.resolve(Promise.resolve(3))
+  const p3 = Promise.resolve(Promise.reject(4))
+  p1.then((value) => {
+    console.log('p1', value)
+  })
+  p2.then((value) => {
+    console.log('p2', value)
+  })
+  p3.catch((reason) => {
+    console.log('p3', reason)
+  }) */
+  myPromise.resolve = function (value) {
+    return new myPromise((resolve, reject) => {
+      if (value instanceof myPromise) {
+        value.then(resolve, reject)
+      } else {
+        resolve(value)
+      }
+    })
+  }
+  myPromise.reject = function (reason) {
+    return new myPromise((resolve, reject) => {
+      reject(reason)
+    })
+  }
+  myPromise.all = function (arr) {
+    return new myPromise((resolve, reject) => {
+      var sum = 0 //成功Promise的数量
+      var resultArr = new Array(arr.length) //用来保存所有成功数据的数组
+      arr.forEach((p, index) => {
+        myPromise.resolve(p).then(
+          (value) => {
+            resultArr[index] = value
+            sum++
+            if (sum == arr.length) {
+              resolve(resultArr)
+            }
+          },
+          (reason) => {
+            reject(reason)
+          }
+        )
+      })
+    })
+  }
+  myPromise.race = function (arr) {
+    //看谁最先完成。
+    return new Promise((resolve, reject) => {
+      arr.forEach((p, index) => {
+        myPromise.resolve(p).then(
+          (value) => {
+            resolve(value)
+          },
+          (reason) => {
+            reject(reason)
+          }
+        )
+      })
+    })
+  }
+  myPromise.resolveDelay = function (value, time) {
+    return new myPromise((resolve, reject) => {
+      setTimeout(() => {
+        if (value instanceof myPromise) {
+          value.then(resolve, reject)
+        } else {
+          resolve(value)
+        }
+      }, time)
+    })
+  }
+  myPromise.rejectDelay = function (reason, reason) {
+    return new myPromise((resolve, reject) => {
+      setTimeout(() => {
+        reject(reason)
+      }, time)
+    })
+  }
   //向外暴露Promise函数
   window.myPromise = myPromise
 })(window)
